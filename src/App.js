a507e42ccc8752ae61b6fa3174d775813a8baa0e
Pob <590650@gmail.com>
2018-10-04 19:51:17 +0700

    add Home page and set up react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import './Routes'
 import './App.css';
+import Routes from './Routes';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <body>
-          <h2>Test Font H2</h2>
-          <p>Test font in paragraph</p>
-        </body>
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Scratch</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+        </Navbar>
+        <Routes />
       </div>
     );
   }
