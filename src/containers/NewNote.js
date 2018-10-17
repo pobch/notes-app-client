796f5b55976b998dee12beb5f690e376936c22f1
Pob <590650@gmail.com>
2018-10-17 15:55:29 +0700

    finished Create Note Page with attachment feature

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, ControlLabel} from 'react-bootstrap'
 import { API } from 'aws-amplify'
 import LoaderButton from '../components/LoaderButton'
 import config from '../config'
+import { s3Upload } from '../libs/awsLib'
 import './NewNote.css'
 
 class NewNote extends Component {
@@ -34,10 +35,16 @@ class NewNote extends Component {
     this.setState({ isLoading: true })
 
     try {
-      await this.createNote({ content: this.state.content })
+      const attachment = this.file ? await s3Upload(this.file) : null
+
+      await this.createNote({
+        content: this.state.content,
+        attachment
+      })
+      
       this.props.history.push('/')
     } catch(error) {
-      alert(error) // PR: should be error.message
+      alert(error.message) // My PR: should be error.message
       this.setState({ isLoading: false })
     }
   }
